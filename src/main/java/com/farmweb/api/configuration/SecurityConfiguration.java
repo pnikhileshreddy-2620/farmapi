2456091cf4737c00fddf186b7149b09e77c9072d
pnikhileshreddy-2620 <pnikhileshreddy2620@gmail.com>
2025-02-28 13:56:48 +0530

    POM Version changed. Security file changed

--- src/main/java/com/farmweb/api/configuration/SecurityConfiguration.java
+++ src/main/java/com/farmweb/api/configuration/SecurityConfiguration.java
@@ -1,33 +1,31 @@
 package com.farmweb.api.configuration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/api/customer/**", "/api/vendor/**","/api/admins/**").permitAll() // Allow unauthenticated access to these endpoints
-//                        .requestMatchers("/api/admins/save").hasRole("ADMIN")
-                        .anyRequest().authenticated()  // Require authentication for all other endpoints
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/customer/**", "/api/vendor/**", "/api/admins/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults()); // Enable HTTP Basic Authentication for simplicity; consider more robust methods for production
+                .httpBasic(withDefaults());
 
         return http.build();
     }
@@ -37,9 +35,12 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-
-
-
-
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 }
-
